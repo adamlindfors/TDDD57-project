b023d43397a5159d9602ebc822e59d8ea6dcee99
Adam <adamlindfors@live.se>
2021-03-17 16:10:31 +0100

    Changed project idea, now shooting projectiles from hands

--- fm.js
+++ fm.js
@@ -10,15 +10,6 @@ const canvasCtx = canvasElement.getContext('2d');
     Right elbow = 14
     */
 
-    /* Three js bones list
-    Left hand = 9
-    Right hand = 26
-    Left elbow = 8
-    Right elbow = 25
-    */
-
-
-
 // QUEUE CODE-------------------------------------------------
 
 function Queue() {
@@ -51,35 +42,75 @@ Number.prototype.map = function (in_min, in_max, out_min, out_max) {
   return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+var ammoBox = function() {
+  this.position = new THREE.Vector3(0,-5,0);
+  this.geometry = new THREE.CircleGeometry( 0.4, 18 );
+  this.material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  this.mesh = new THREE.Mesh( this.geometry, this.material );  
+}
+var ammobox = new ammoBox();
+scene.add(ammobox.mesh);
+console.log(ammobox);
 
-
-function updateBones(lndmrks){
-  var posVec = new THREE.Vector3(0,0,0);
-  var mappedX = 0;
-  var mappedY = 0;
-  //Right hand 
-  mappedX = lndmrks[16].x.map(1, 0, rightMostPosLocal.x, leftMostPosLocal.x);
-  mappedY = -lndmrks[16].y.map(0, 1, -1, 1);
-  //posVec = model.children[0].children[0].skeleton.bones[26].position.set(mappedX, mappedY, 0 );
-  model.children[0].children[0].skeleton.bones[26].position.set(mappedX, leftMostPosLocal.y, 2);
-  console.log(lndmrks[16].x);
-  //Left hand  
-  //posVec = model.children[0].children[0].skeleton.bones[9].worldToLocal(new THREE.Vector3(lndmrks[15].x*10,lndmrks[15].y*10,  2));
-  //model.children[0].children[0].skeleton.bones[9].position.set(posVec.x, posVec.y, 2);
-  // Right forearm 25
-  mappedX = lndmrks[14].x.map(0, 1, leftMostPosLocal.x, rightMostPosLocal.x);
-  mappedY = -lndmrks[14].y.map(0, 1, -1, 1);
-  //posVec = model.children[0].children[0].skeleton.bones[25].position.set(mappedX, mappedY, 0 );
-  model.children[0].children[0].skeleton.bones[25].position.set(mappedX, leftMostPosLocal.y, 2);
-  // Left forearm 8
-  //model.children[0].children[0].skeleton.bones[8].position.set(lndmrks[13].x*10, lndmrks[13].y*10, lndmrks[13].z);
-  //console.log(lndmrks[16].y);
-  //var bla = model.children[0].children[0].skeleton.bones[25].position;
-  //var test = model.children[0].children[0].skeleton.bones[26].localToWorld(new THREE.Vector3(bla.x, bla.y, bla.z));
-  //console.log(test);
+var particle = function(material){
+  this.ammoGeometry = new THREE.SphereGeometry(0.09, 6, 6);
+  this.ammoMaterial = material;
+  this.mesh = new THREE.Mesh(this.ammoGeometry, this.ammoMaterial);
+  this.position = new THREE.Vector3(100,100,100);
+  this.velocity = new THREE.Vector3(0.0,0.0,0.0);
+  this.lifeLength = Math.random();
+  this.mesh.position.set(this.position.x, this.position.y, this.position.z);
+}
+particle.prototype = {
+  respawn: function(startPos, forearmLandmark){
+    if(this.lifeLength < 0.0){
+      this.position.set(startPos.x, startPos.y, startPos.z);
+      this.mesh.position.set(startPos.x, startPos.y, startPos.z);
+      this.lifeLength = 5.0;
+      var tempVel = new THREE.Vector3(startPos.x - forearmLandmark.x, 
+                                      startPos.y - -(forearmLandmark.y), 
+                                      0.0);
+      tempVel = tempVel.normalize().multiplyScalar(5);
+      this.velocity.set(tempVel.x, tempVel.y, tempVel.z);
+    }
+  },
+  update: function() {
+    var tempVel = new THREE.Vector3(this.velocity.x, this.velocity.y, this.velocity.z);
+    tempVel.y += -9.82*0.01;
+    this.velocity.set(tempVel.x, tempVel.y, tempVel.z);
+    this.mesh.position.add(tempVel.multiplyScalar(0.03));
+  }
 }
 
+var handCannon = function(mesh, material, side){
+  this.side = side;
+	this.position = new THREE.Vector3(0,0,0);
+  this.numberOfAmmo = 100;
+  this.mesh = mesh;
+  this.ammo = [];
+  this.ammoMaterial = material;
+}
+handCannon.prototype = {
+  loadParticles: function(){
+    for(var i = 0; i < this.numberOfAmmo; i++){
+      this.ammo.push(new particle(this.ammoMaterial));
+      scene.add(this.ammo[this.ammo.length - 1].mesh);
+    }
+  },
+  moveCannon: function(handLandmark){
+    this.position.set(handLandmark.x, -handLandmark.y, 0);
+    this.mesh.position.set(handLandmark.x, -handLandmark.y, 0);
+  }
+}
 
+var leftHandCannon = new handCannon(leftSphere, leftMaterial , "left");
+var rightHandCannon = new handCannon(rightSphere, rightMaterial , "right");
+rightHandCannon.loadParticles();
+leftHandCannon.loadParticles();
+var cannons = [];
+cannons.push(rightHandCannon);
+cannons.push(leftHandCannon);
+var ammotimer = 0;
 
 function onResults(results) {
   canvasCtx.save();
@@ -90,10 +121,39 @@ function onResults(results) {
                  {color: '#00FF00', lineWidth: 4});
   drawLandmarks(canvasCtx, results.poseLandmarks,
                 {color: '#FF0000', lineWidth: 2});
-  if(model){
-    updateBones(results.poseLandmarks);
+  leftHandCannon.moveCannon(results.poseLandmarks[15]);
+  rightHandCannon.moveCannon(results.poseLandmarks[16]);
+  for(var i = 0; i < cannons.length; i++){
+    for(var j = 0; j < cannons[i].ammo.length; j++){
+      cannons[i].ammo[j].lifeLength -= 0.01;
+      if(cannons[i].numberOfAmmo > 0 && cannons[i].ammo[j].lifeLength <= 0.0){
+        if(cannons[i].side == "right") {
+          cannons[i].ammo[j].respawn(cannons[i].position, results.poseLandmarks[14]);
+        }
+        else {
+          cannons[i].ammo[j].respawn(cannons[i].position, results.poseLandmarks[13]);
+        }
+        cannons[i].numberOfAmmo -=1;
+        if(cannons[i].numberOfAmmo <= 0) {
+          cannons[i].numberOfAmmo = 0;
+        }
+      }
+      cannons[i].ammo[j].update();
+    }
   }
+  /*for(var i = 0; i < rightHandCannon.ammo.length; i++) {
+    if(rightHandCannon.numberOfAmmo > 0 && rightHandCannon.ammo[i].lifeLength <= 0.0) {
+      rightHandCannon.ammo[i].respawn(rightHandCannon.position, results.poseLandmarks[14]);
+      rightHandCannon.numberOfAmmo -= 1;
+      if(rightHandCannon.numberOfAmmo <= 0) {
+        rightHandCannon.numberOfAmmo = 0;
+      }
+    }
+    rightHandCannon.ammo[i].update();
+  }*/
 }
+
+
 
 
 const holistic = new Holistic({locateFile: (file) => {
