60318178e57560d4034904973f11f6dafecabc64
Adam <adamlindfors@live.se>
2021-03-19 16:13:59 +0100

    All monsters added with sound effects

--- fm.js
+++ fm.js
@@ -1,10 +1,19 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
-const textureLoader = new THREE.TextureLoader()
-textureLoader.crossOrigin = "Anonymous"
-const ammoTexture = textureLoader.load("battery_white.png")
-
+const textureLoader = new THREE.TextureLoader();
+textureLoader.crossOrigin = "Anonymous";
+const ammoTexture = textureLoader.load("images/battery_white.png");
+const blueMonsterTexture1 = textureLoader.load("images/blue-monster1.png");
+const blueMonsterTexture2 = textureLoader.load("images/blue-monster2.png");
+const blueMonsterTexture3 = textureLoader.load("images/blue-monster3.png");
+const redMonsterTexture1 = textureLoader.load("images/red-monster1.png");
+const redMonsterTexture2 = textureLoader.load("images/red-monster2.png");
+const redMonsterTexture3 = textureLoader.load("images/red-monster3.png");
+const leftHandTexture = textureLoader.load("images/left-hand.png");
+const rightHandTexture = textureLoader.load("images/right-hand.png");
+var monstereDeath = new Audio('Audio/monsterDeath.wav');
+var cannonFire = new Audio('Audio/cannon.flac');
 
     /* Mediapipe Pose list 
     Left hand = 15
@@ -12,46 +21,106 @@ const ammoTexture = textureLoader.load("battery_white.png")
     Left elbow = 13
     Right elbow = 14
     */
-var monster = function(){
-  this.position = new THREE.Vector3(10, 0, 0);
+
+var monster = function(texture, color){
+  this.position = new THREE.Vector3(Math.random() * (10 - 7) + 7, Math.random() * (2 - (-2)) -2, 0);
   this.velocity = new THREE.Vector3(-1,0,0);
-  this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  this.material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+  this.geometry = new THREE.PlaneGeometry( 1.0, 1.0, 1.0 );
+  this.material = new THREE.MeshBasicMaterial( {map: texture, alphaTest: 0.5} );
   this.mesh = new THREE.Mesh( this.geometry, this.material );
   this.mesh.position.set(this.position.x, this.position.y, this.position.z);
-  this.boundingBox = new THREE.Box3();
+  this.box = new THREE.Box3();
   this.mesh.geometry.computeBoundingBox();
+  this.life = 30;
+  this.color = color;
 }
 monster.prototype = {
   update: function(){
     var tempVel = new THREE.Vector3(this.velocity.x, this.velocity.y, this.velocity.y);
     this.position.add(tempVel.multiplyScalar(0.03));
     this.mesh.position.set(this.position.x, this.position.y, this.position.z);
-  }
+    this.box.copy( this.mesh.geometry.boundingBox ).applyMatrix4( this.mesh.matrixWorld );
+  },
+  hitByPlayer: function(myParticle){
+    
+    if(myParticle.box.intersectsBox(this.box)) {
+      if(this.color == "blue" && myParticle.ammoMaterial.color.b > 0.5) {
+      this.life -= 1;
+      myParticle.position.set(100,100,100);
+      myParticle.mesh.position.set(100,100,100);
+      }
+      else if(this.color == "red" && myParticle.ammoMaterial.color.r > 0.5) {
+        this.life -= 1;
+        myParticle.position.set(100,100,100)
+        myParticle.mesh.position.set(100,100,100);
+        }
+    }
+    if(this.life <= 0) {
+      monstereDeath.play();
+      this.respawn();
+      this.life = 30;
+    }
+  },
+  respawn: function() {
+    this.position.set(Math.random() * (17 - 10) + 10, Math.random() * (2 - (-2)) -2, 0);
+    this.velocity.x = Math.random() * (-1 - -(-1.5)) - 1.5;
+  },
+
+
 }
-var monster1 = new monster();
+var monster1 = new monster(blueMonsterTexture1, "blue");
+var monster2 = new monster(blueMonsterTexture2, "blue");
+var monster3 = new monster(blueMonsterTexture3, "blue");
+var monster4 = new monster(redMonsterTexture1, "red");
+var monster5 = new monster(redMonsterTexture2, "red");
+var monster6 = new monster(redMonsterTexture3, "red");
 scene.add(monster1.mesh);
+scene.add(monster2.mesh);
+scene.add(monster3.mesh);
+scene.add(monster4.mesh);
+scene.add(monster5.mesh);
+scene.add(monster6.mesh);
+var monsters = [];
+monsters.push(monster1);
+monsters.push(monster2);
+monsters.push(monster3);
+monsters.push(monster4);
+monsters.push(monster5);
+monsters.push(monster6);
+console.log(monster6)
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
 var ammoBox = function() {
   this.position = new THREE.Vector3(0,-1,0);
   this.radius = 0.4;
-  this.red = 0;
-  this.green = 1;
-  this.blue = 0;
+  this.red = 220/255;
+  this.green = 255/255;
+  this.blue = 180/255;
   this.scale = 1.0;
   this.geometry = new THREE.CircleGeometry( this.radius, 18 );
-  this.material = new THREE.MeshBasicMaterial( { map:ammoTexture, color:"rgb(${this.red}, ${this.green}, ${this.blue})" } );
+  this.material = new THREE.MeshBasicMaterial( { map:ammoTexture, color:"rgb(${this.red}, ${this.green}, ${this.blue})", alphaTest: 0.5 } );
   this.mesh = new THREE.Mesh( this.geometry, this.material );  
   this.mesh.position.set(this.position.x, this.position.y, this.position.z);
   this.box = new THREE.Box3();
   this.mesh.geometry.computeBoundingBox();
 }
 ammoBox.prototype = {
-  giveAmmo: function(myCannon) {
-        this.red += 0.05*myCannon.rgb.x;
-        this.green -= 0.05*this.green;
-        this.blue += 0.05*myCannon.rgb.z;
+  giveAmmo: async function(myCannon) {
+        if(myCannon.rgb.x == 0) {
+          this.red -= 0.1*this.red;
+        } else if(myCannon.rgb.x == 1) {
+          this.red += 0.1*this.red;
+        }
+        this.green -= 0.1*this.green;
+        if(myCannon.rgb.z == 0) {
+          this.blue -= 0.1*this.blue;
+        } else if(myCannon.rgb.z == 1) {
+          this.blue += 0.1*this.blue;
+        }
         this.scale += 0.005;
         if(this.scale > 1.1){
           this.scale = 1.1;
@@ -65,15 +134,26 @@ ammoBox.prototype = {
         this.material.color.setRGB(this.red, this.green, this.blue);
         this.mesh.scale.set(this.scale, this.scale, this.scale);
         if(this.red == 1.0 || this.blue == 1.0) {
+          await sleep(1500);
           myCannon.numberOfAmmo = 100;
         }
         
   }
 }
 
+var ammoHand = function(pos, texture) {
+  this.geometry = new THREE.PlaneGeometry( 1, 1, 1 );
+  this.material = new THREE.MeshBasicMaterial( { map: texture} );
+  this.mesh = new THREE.Mesh(this.geometry, this.material);
+  this.mesh.position.set(pos.x, pos.y, pos.z);
+  scene.add(this.mesh);
+}
 
-var test = new ammoBox();
-scene.add(test.mesh);
+var leftAmmoHand = new ammoHand(new THREE.Vector3(-2, 5, -3), leftHandTexture);
+var rightAmmoHand = new ammoHand(new THREE.Vector3(0, 5, -3), rightHandTexture);
+
+var myAmmoBox = new ammoBox();
+scene.add(myAmmoBox.mesh);
 
 var particle = function(material){
   this.ammoGeometry = new THREE.SphereGeometry(0.09, 6, 6);
@@ -83,6 +163,8 @@ var particle = function(material){
   this.velocity = new THREE.Vector3(0.0,0.0,0.0);
   this.lifeLength = Math.random();
   this.mesh.position.set(this.position.x, this.position.y, this.position.z);
+  this.box = new THREE.Box3();
+  this.mesh.geometry.computeBoundingBox();
 }
 particle.prototype = {
   respawn: function(startPos, forearmLandmark){
@@ -95,6 +177,12 @@ particle.prototype = {
                                       0.0);
       tempVel = tempVel.normalize().multiplyScalar(5);
       this.velocity.set(tempVel.x, tempVel.y, tempVel.z);
+      if(cannonFire.paused) {
+        cannonFire.play();
+      } else{
+        cannonFire.currentTime = 0;
+      }
+      
     }
   },
   update: function() {
@@ -102,6 +190,7 @@ particle.prototype = {
     tempVel.y += -9.82*0.01;
     this.velocity.set(tempVel.x, tempVel.y, tempVel.z);
     this.mesh.position.add(tempVel.multiplyScalar(0.03));
+    this.box.copy( this.mesh.geometry.boundingBox ).applyMatrix4( this.mesh.matrixWorld );
   }
 }
 
@@ -167,24 +256,38 @@ function onResults(results) {
         }
       }
       cannons[i].ammo[j].update();
+      for(var k = 0; k < monsters.length; k++){
+        monsters[k].hitByPlayer(cannons[i].ammo[j]);
+      }
     }
   }
-  if(test.box.intersectsBox(rightHandCannon.box) && !test.box.intersectsBox(leftHandCannon.box)){
-    test.giveAmmo(rightHandCannon);
-  } else if(test.box.intersectsBox(leftHandCannon.box) && !test.box.intersectsBox(rightHandCannon.box)) {
-    test.giveAmmo(leftHandCannon);
-  } else if(!test.box.intersectsBox(leftHandCannon.box) && !test.box.intersectsBox(rightHandCannon.box)) {
-    test.mesh.scale.set(1.0,1.0,1.0);
-    test.red = 0;
-    test.green = 1;
-    test.blue = 0;
-    test.scale = 1.0;
-    test.material.color.setRGB(test.red, test.green, test.blue);
+  if(myAmmoBox.box.intersectsBox(rightHandCannon.box) && !myAmmoBox.box.intersectsBox(leftHandCannon.box)){
+    myAmmoBox.giveAmmo(rightHandCannon);
+  } else if(myAmmoBox.box.intersectsBox(leftHandCannon.box) && !myAmmoBox.box.intersectsBox(rightHandCannon.box)) {
+    myAmmoBox.giveAmmo(leftHandCannon);
+  } else if(!myAmmoBox.box.intersectsBox(leftHandCannon.box) && !myAmmoBox.box.intersectsBox(rightHandCannon.box)) {
+    myAmmoBox.mesh.scale.set(1.0,1.0,1.0);
+    myAmmoBox.red = 220/255;
+    myAmmoBox.green = 255/255;
+    myAmmoBox.blue = 180/255;
+    myAmmoBox.scale = 1.0;
+    myAmmoBox.material.color.setRGB(myAmmoBox.red, myAmmoBox.green, myAmmoBox.blue);
   }
-  
-  test.box.copy( test.mesh.geometry.boundingBox ).applyMatrix4( test.mesh.matrixWorld );
+  myAmmoBox.box.copy( myAmmoBox.mesh.geometry.boundingBox ).applyMatrix4( myAmmoBox.mesh.matrixWorld );
   monster1.update();
-  console.log(monster1.position);
+  monster2.update();
+  monster3.update();
+  monster4.update();
+  monster5.update();
+  monster6.update();
+  rightAmmoHand.material.color.setRGB(0, 0, rightHandCannon.numberOfAmmo/100);
+  if(rightAmmoHand.material.color.b == 0) {
+    rightAmmoHand.material.color.setRGB(1,1,1);
+  }
+  leftAmmoHand.material.color.setRGB(leftHandCannon.numberOfAmmo/100, 0, 0);
+  if(leftAmmoHand.material.color.r == 0) {
+    leftAmmoHand.material.color.setRGB(1,1,1);
+  }
 }
 
 
